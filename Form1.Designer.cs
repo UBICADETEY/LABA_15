4e5711fad8c06ad4dc373e602375482318295de4
микола тесла <chemurziev.mikail@mail.ru>
2021-05-17 20:02:17 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace LabRab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnChooseFont = new System.Windows.Forms.Button();
+            this.panelChooseColor = new System.Windows.Forms.Panel();
+            this.btnChooseFC = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtTerms = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseColor.Location = new System.Drawing.Point(35, 40);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(157, 26);
+            this.btnChooseColor.TabIndex = 0;
+            this.btnChooseColor.Text = "Choose Color";
+            this.btnChooseColor.UseVisualStyleBackColor = true;
+            this.btnChooseColor.Click += new System.EventHandler(this.btnChooseColor_Click);
+            // 
+            // btnChooseFont
+            // 
+            this.btnChooseFont.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseFont.Location = new System.Drawing.Point(35, 72);
+            this.btnChooseFont.Name = "btnChooseFont";
+            this.btnChooseFont.Size = new System.Drawing.Size(157, 27);
+            this.btnChooseFont.TabIndex = 1;
+            this.btnChooseFont.Text = "Choose Font";
+            this.btnChooseFont.UseVisualStyleBackColor = true;
+            this.btnChooseFont.Click += new System.EventHandler(this.btnChooseFont_Click);
+            // 
+            // panelChooseColor
+            // 
+            this.panelChooseColor.Location = new System.Drawing.Point(230, 40);
+            this.panelChooseColor.Name = "panelChooseColor";
+            this.panelChooseColor.Size = new System.Drawing.Size(190, 26);
+            this.panelChooseColor.TabIndex = 2;
+            // 
+            // btnChooseFC
+            // 
+            this.btnChooseFC.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChooseFC.Location = new System.Drawing.Point(35, 105);
+            this.btnChooseFC.Name = "btnChooseFC";
+            this.btnChooseFC.Size = new System.Drawing.Size(157, 28);
+            this.btnChooseFC.TabIndex = 4;
+            this.btnChooseFC.Text = "Choose Font Color";
+            this.btnChooseFC.UseVisualStyleBackColor = true;
+            this.btnChooseFC.Click += new System.EventHandler(this.btnChooseFC_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalc.Location = new System.Drawing.Point(230, 218);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(190, 79);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtTerms
+            // 
+            this.txtTerms.Location = new System.Drawing.Point(230, 149);
+            this.txtTerms.Name = "txtTerms";
+            this.txtTerms.Size = new System.Drawing.Size(190, 20);
+            this.txtTerms.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter the number of terms";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(230, 192);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(190, 20);
+            this.txtAnswer.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Answer";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(92, 248);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Enter X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 309);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTerms);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnChooseFC);
+            this.Controls.Add(this.panelChooseColor);
+            this.Controls.Add(this.btnChooseFont);
+            this.Controls.Add(this.btnChooseColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnChooseFont;
+        private System.Windows.Forms.Panel panelChooseColor;
+        private System.Windows.Forms.Button btnChooseFC;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabRab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtChooseFont_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.ShowHelp = true;
+            dlg.Color = panelChooseColor.BackColor;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                BackColor = dlg.Color;
+            }
+        }
+
+        private void btnChooseFont_Click(object sender, EventArgs e)
+        {
+            FontDialog dlg = new FontDialog();
+            dlg.ShowColor = true;
+            dlg.ShowHelp = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                btnChooseFont.Font = dlg.Font;
+                
+                btnChooseColor.Font = dlg.Font;
+                
+                btnChooseFC.Font = dlg.Font;
+
+                btnCalc.Font = dlg.Font;
+
+                label1.Font = dlg.Font;
+
+                label2.Font = dlg.Font;
+
+                label3.Font = dlg.Font;
+
+                txtTerms.Font = dlg.Font;
+
+                txtAnswer.Font = dlg.Font;
+
+                textBoxX.Font = dlg.Font;
+            }
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseFC_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.FullOpen = true;
+            dlg.ShowHelp = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                btnChooseFont.ForeColor = dlg.Color;
+
+                btnChooseColor.ForeColor = dlg.Color;
+
+                btnChooseFC.ForeColor = dlg.Color;
+
+                btnCalc.ForeColor = dlg.Color;
+
+                label1.ForeColor = dlg.Color;
+
+                label2.ForeColor = dlg.Color;
+
+                label3.ForeColor = dlg.Color;
+
+                txtTerms.ForeColor = dlg.Color;
+
+                txtAnswer.ForeColor = dlg.Color;
+
+                textBoxX.ForeColor = dlg.Color;
+            }
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBoxX.Text);
+            int n = Convert.ToInt32(txtTerms.Text);
+            double ans = 0;
+            double item;
+            int k = 2;
+            double fac = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    fac *= j;
+                }
+
+                item = (i % 2 == 0) ? (Math.Sin(Math.Pow(x, i)) * Math.Pow(x, k)) / fac : (Math.Pow(x, i) * Math.Sin(Math.Pow(x, k))) / fac;
+                ans += item;
+                k++;
+                fac = 1;
+            }
+            txtAnswer.Text = ans.ToString();
+
+        }
+    }
+}
